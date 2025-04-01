e9394edd82ffbb66038d3fbf820a40e9c06bfad0
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2025-04-01 19:50:38 +0600

    Refactor app.ts: Reorder imports and clean up whitespace

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import path from 'path';
 import globalErrorHandler from './app/errors/globalErrorHandler';
-import routes from './app/routes';
 import notFound from './app/middleware/notFound';
+import routes from './app/routes';
 import requestLogger from './app/utils/logger/requestLogger';
 
 const app: Application = express();
@@ -34,8 +34,6 @@ app.get('/', async (req: Request, res: Response) => {
 app.get('/health', async (req: Request, res: Response) => {
   res.status(200).json({ message: 'API is running' });
 });
-
-
 
 //Not found route
 app.use(notFound);
